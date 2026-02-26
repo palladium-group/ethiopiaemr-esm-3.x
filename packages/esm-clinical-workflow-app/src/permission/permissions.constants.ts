67a4c559620812b7e1a323b7138207dd5ba0bab4
Ermias Tefera <ermias.abera@thepalladiumgroup.com>
2026-02-26 15:27:31 +0300

    feat: Update permissions and enhance triage page functionality
    - Refactored permissions in `permissions.constants.ts` to include common and triage-specific permissions.
    - Integrated `UserHasAccess` component in `CentralTriagePage` and `EmergencyTriagePage` to conditionally render the "New Patient" button based on user permissions.

--- packages/esm-clinical-workflow-app/src/permission/permissions.constants.ts
+++ packages/esm-clinical-workflow-app/src/permission/permissions.constants.ts
@@ -1,5 +1,6 @@
 export enum Permissions {
-  // EthiopiaEMR permissions
+  // Common permissions
+  RegisterNewPatient = 'o3: Register Patient',
 
   // Encounter permissions
   ViewEncounter = 'o3: View Encounters',
@@ -17,4 +18,8 @@ export enum Permissions {
   AddLabOrder = 'o3: View Test Order Form',
   AddImagingOrder = 'o3: View Imaging Order Form',
   AddProcedureOrder = 'o3: View Patient Chart Order Dashboard',
+
+  // Triage Permissions
+  ViewTriage = 'ethioemr: Triage: View Triage',
+  AddTriageForm = 'ethioemr: Triage: Add Triage Form',
 }
