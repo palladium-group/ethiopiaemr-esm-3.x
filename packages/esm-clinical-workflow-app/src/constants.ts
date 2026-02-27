914d39244787b28fcc905a5ee8b39f032a118d82
Yonas Alemseged <yonialem3@gmail.com>
2026-02-27 15:45:36 +0300

    Test commit

--- packages/esm-clinical-workflow-app/src/constants.ts
+++ packages/esm-clinical-workflow-app/src/constants.ts
@@ -4,3 +4,5 @@ export const spaBasePath = `${window.spaBase}/home`;
 // Pagination constants
 export const DEFAULT_PAGE_SIZE = 25;
 export const DEFAULT_PAGE_SIZES = [10, 25, 50, 100];
+// Patient registration constants
+export const GIVEN_NAME_LABEL = 'Given Name';
