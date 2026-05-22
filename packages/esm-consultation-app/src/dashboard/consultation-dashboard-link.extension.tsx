fbc282a387859d1d724edfd29916e2428c359944
ahmihassu <ahmihassu@gmail.com>
2026-05-22 12:32:09 +0300

    Add consultation nav link to patient chart

---
+++ packages/esm-consultation-app/src/dashboard/consultation-dashboard-link.extension.tsx
@@ -0,0 +1,7 @@
+import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
+
+// t('consultation', 'Consultation')
+export default createDashboardLink({
+  path: 'consultation',
+  title: 'Consultation',
+});
--- packages/esm-consultation-app/src/index.ts
+++ packages/esm-consultation-app/src/index.ts
@@ -1,9 +1,17 @@
-import { defineConfigSchema } from '@openmrs/esm-framework';
+import { defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
+import ConsultationDashboardLink from './dashboard/consultation-dashboard-link.extension';
 import { configSchema } from './config-schema';
 import { moduleName } from './constants';
 
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
+const options = {
+  featureName: 'consultation',
+  moduleName,
+};
+
 export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
 }
+
+export const consultationDashboardLink = getSyncLifecycle(ConsultationDashboardLink, options);
