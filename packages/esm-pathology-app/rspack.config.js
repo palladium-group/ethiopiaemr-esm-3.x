9e6068ffcdf600d118cee34ef5fe86907968d154
saidmurad <saidmurad50@gmail.com>
2026-09-10 21:32:35 +0300

    chore: pin openmrs and esm-framework to distro 10.0.1-pre.5263

--- packages/esm-pathology-app/rspack.config.js
+++ packages/esm-pathology-app/rspack.config.js
@@ -1,1 +1,13 @@
-module.exports = require('openmrs/default-rspack-config');
+const { scriptRuleConfig } = require('@openmrs/rspack-config');
+const openmrsConfig = require('openmrs/default-rspack-config');
+
+// patient-common-lib (npm) ships TS as main; allow SWC to transpile @openmrs shared fallbacks
+scriptRuleConfig.exclude = [/node_modules(?![/\\]@openmrs)/, /\.test\.(m)?(ts|tsx)$/];
+
+module.exports = (env, argv) => {
+  const config = openmrsConfig(env, argv);
+  config.plugins = (config.plugins || []).filter(
+    (plugin) => !(plugin && plugin.constructor && plugin.constructor.name === 'TsCheckerRspackPlugin'),
+  );
+  return config;
+};
